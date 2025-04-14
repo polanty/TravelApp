24bfc607b11d25dbd3328e8f7a683a49ef19ac82
polanty <Tijaniabiola1599@gmail.com>
2025-04-14 15:56:14 -0700

    implementing error routes

--- app.js
+++ app.js
@@ -27,4 +27,12 @@ app.use('/api/v1/tours', tourRouter);
 //Routes to the user aspects
 app.use('/api/v1/users', userRouter);
 
+//Handles all unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
