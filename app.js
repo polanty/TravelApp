e4a5fba65fad632cf2364473d07db92f6c723ad9
polanty <Tijaniabiola1599@gmail.com>
2025-04-16 15:57:22 -0700

    created global error handling

--- app.js
+++ app.js
@@ -29,10 +29,28 @@ app.use('/api/v1/users', userRouter);
 
 //Handles all unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
 });
 
+//Error handling middleware
+//handles any other error not caught in program functionality
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
